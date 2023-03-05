06efc0f0d1678acb414c01fc69b5d85f5fae2f5d
Danylo Kharkiv <donatelod12@gmail.com>
2023-03-05 21:55:56 +0200

    up

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,7 +4,7 @@ export const fetchImages = async (query, page) => {
   const response = await axios.get(`https://pixabay.com/api/`, {
     method: 'get',
     params: {
-      key: '31011594-1ff7c9f95cd2b442eeb7c007c',
+      key: '33043171-06b34b60f3bfebeb66d48d21f',
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
